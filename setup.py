97e9088c9b1c08d5b7f6438732ceea249b05c8c7
Ebram Rafat <ebramrafat653@gmail.com>
2026-05-23 02:34:55 +0300

    build: add setup.py to package the project modules

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="california_housing_full_project",
+    version="0.1.0",
+    packages=find_packages(),
+    description="California Housing MLOps Project",
+    author="Ebram Rafat",
+)
