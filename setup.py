cebf1b74eeead766b635183439f1ec938bb89bf8
Ebram Rafat <ebramrafat653@gmail.com>
2026-08-29 17:15:42 +0300

    chore: remove legacy requirements.txt and setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="california_housing_full_project",
-    version="0.1.0",
-    packages=find_packages(),
-    description="California Housing MLOps Project",
-    author="Ebram Rafat",
-)
